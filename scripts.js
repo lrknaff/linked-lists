07f789a37e73f02813eb6c4d9db91b160d5e8999
Lacey Knaff <lacey.knaff@gmail.com>
2016-08-31 13:43:24 -0600

    getting number counter to work

--- scripts.js
+++ scripts.js
@@ -34,21 +34,49 @@ function addAnotherBookmark () {
   $('.bookmark-info').attr('id', function(i) {
      return 'bookmark'+(i+1);
   });
-  $(titleInput).text('');
 }
 };
 
+//function to count number of bookmarks
+function getNumberOfBookmarks () {
+  var numberOfBookmarks = $('.bookmark-info').length;
+};
+
+//function to display number of bookmarks
+function displayNumberOfBookmarks () {
+  var numberOfBookmarks = $('.bookmark-info').length;
+  $('.number-of-bookmarks').text(numberOfBookmarks);
+}
+
+//function to count number of read bookmarks
+function getNumberOfRead () {
+  var numberOfRead = $('.read').length;
+}
+
+//function to display number of read
+function displayNumberOfRead () {
+  var numberOfRead = $('.read').length;
+  $('.number-of-read').text(numberOfRead);
+};
+
+//function to clear input field and disable button if both fields have content
+function clearInputFields () {
+  var titleInput = $('#title-input').val();
+  var urlInput = $('#url-input').val();
+
+  if (urlInput.length > 0 && titleInput.length > 0) {
+    $('#create-bookmark-btn').prop("disabled", true);
+    $('#title-input').val('');
+    $('#url-input').val('');
+    $('.message').text('');
+  }
+}
 
 //enable create bookmark button when user types in title or url field
 $('#title-input, #url-input').keyup(function () {
   $('#create-bookmark-btn').prop("disabled", false);
 });
 
-//function to clear field on submit bookmark click
-function clearInputFields () {
-  $('create-bookmark-btn').prop("disabled", true);
-
-};
 
 //post new bookmark
 $('#create-bookmark-btn').on('click', function (){
@@ -56,14 +84,19 @@ $('#create-bookmark-btn').on('click', function (){
   getUrlInput();
   addAnotherBookmark();
   clearInputFields();
+  getNumberOfBookmarks();
+  displayNumberOfBookmarks();
 });
 
 //add class read on button click
 $('.create-bookmark').on('click', '#markasread-btn', function (){
   $(this).parent().toggleClass('read');
+  getNumberOfRead();
+  displayNumberOfRead();
 });
 
 //remove bookmark
 $('.create-bookmark').on('click', '#remove-btn', function (){
   $(this).parent().remove();
+displayNumberOfBookmarks();
 })
