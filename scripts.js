08e846cd3dbbb56dd0f2696e9f564a0977b3795a
Lacey Knaff <lacey.knaff@gmail.com>
2016-08-31 09:03:37 -0600

    add read class working

--- scripts.js
+++ scripts.js
@@ -15,17 +15,41 @@ function addAnotherBookmark () {
   var urlInput = $('#url-input').val();
   //add new bookmark to list
   $('.create-bookmark').append(`
-      <article>
+      <article class="bookmark-info">
         <h2>${titleInput}</h2>
         <a href="#">${urlInput}</a>
-        <button>Mark as Read</button>
-        <button>Remove</button>
+        <button id="markasread-btn">Mark as Read</button>
+        <button id="remove-btn">Remove</button>
       </article>`);
 };
 
-$('#create-bookmark-btn').click(function (){
+//function to add 'read' class on bookmark
+function markAsRead () {
+  $('.bookmark-info').addClass('read');
+};
+
+// //function to remove class 'read' on bookmark
+// function removeMarkRead () {
+//   $('.bookmark-info').removeClass('read');
+// };
+//
+//function to remove bookmark when you click remove button
+function removeBookmark () {
+  var bookmarkInfo = $('.bookmark-info').val();
+  $('remove-btn').remove(bookmarkInfo);
+};
+
+$('#create-bookmark-btn').on('click', function (){
   getTitleInput();
   getUrlInput();
   addAnotherBookmark();
 
+});
+
+$('.create-bookmark').on('click', '#markasread-btn', function (){
+  markAsRead();
+});
+
+$('#remove-btn').on ('click', function (){
+  removeBookmark();
 });
