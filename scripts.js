eda4030c97241c018b937f6830e4345bdf135665
Lacey Knaff <lacey.knaff@gmail.com>
2016-09-01 09:00:00 -0600

    html css and javascript finalized

--- scripts.js
+++ scripts.js
@@ -1,111 +1,64 @@
-
+//functions to get title and URL input
 function getTitleInput () {
   var titleInput = $('#title-input').val();
-  console.log(titleInput);
-}
-
+};
 function getUrlInput () {
   var urlInput = $('#url-input').val();
-  console.log(urlInput);
-}
-
-
-
+};
+//function to add new bookmark to list
 function addAnotherBookmark () {
   var titleInput = $('#title-input').val();
   var urlInput = $('#url-input').val();
-
-  //if no url gives error message
-  if (urlInput === '') {
-    $('.message').text('oops! you must input a valid url');
-  }
-
-  else {
-//add new bookmark to list
   $('.number-counter').append(`
-      <article class="bookmark-info">
-        <h2>${titleInput}</h2>
-        <a href="${urlInput}">${urlInput}</a>
-        <div id="buttons"><button id="markasread-btn">Mark as Read</button>
-        <button id="remove-btn">Remove</button></div>
-      </article>`);
-
-  //function to add unique id for each bookmark
-  $('.bookmark-info').attr('id', function(i) {
-     return 'bookmark'+(i+1);
-  });
-}
+    <article class="bookmark-info">
+            <h2>${titleInput}</h2>
+            <a href="${urlInput}">${urlInput}</a>
+            <div id="buttons"><button id="markasread-btn">Mark as Read</button>
+            <button id="remove-btn">Remove</button></div>
+          </article>`);
 };
 
 
-//function to test if URL is valid
+// function to test if URL is valid
 function testValidUrl () {
   var urlInput = $('#url-input').val();
   return urlInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
 };
-
-
-//function to count number of bookmarks
+// //function to count number of bookmarks
 function getNumberOfBookmarks () {
   var numberOfBookmarks = $('.bookmark-info').length;
 };
-
 //function to display number of bookmarks
 function displayNumberOfBookmarks () {
   var numberOfBookmarks = $('.bookmark-info').length;
   $('.number-of-bookmarks p').text(numberOfBookmarks);
-}
-
+};
 //function to count number of read bookmarks
 function getNumberOfRead () {
   var numberOfRead = $('.bookmark-info.read').length;
-}
-
-//function to display number of read
+};
+//function to display number of read bookmarks
 function displayNumberOfRead () {
   var numberOfRead = $('.bookmark-info.read').length;
   $('.number-of-read p').text(numberOfRead);
 };
-
-//function to count number of unread
+//function to count number of unread bookmarks
 function displayNumberOfUnread () {
   var numberOfUnread = $('.bookmark-info:not(.read)').length;
   $('.number-of-unread p').text(numberOfUnread);
 };
-
-// function displayNumberOfUnread () {
-//   $('.number-of-unread').text();
-// }
-
 //function to clear input field and disable button if both fields have content
 function clearInputFields () {
   var titleInput = $('#title-input').val();
   var urlInput = $('#url-input').val();
-
   if (urlInput.length > 0 && titleInput.length > 0) {
     $('#create-bookmark-btn').prop("disabled", true);
     $('#title-input').val('');
     $('#url-input').val('');
     $('.message').text('');
   }
-}
-
-//enable create bookmark button when user types in title or url field
-$('#title-input, #url-input').keyup(function () {
-  $('#create-bookmark-btn').prop("disabled", false);
-});
-
-
-//post new bookmark
-$('#create-bookmark-btn').on('click', function (){
-  console.log(testValidUrl())
-  if(testValidUrl()) {
-    submitBookmarkEvents()
-  } else {
-    alert('Please enter a valid URL');
-  }
-});
-
+};
+//function that binds together submit bookmark events
 function submitBookmarkEvents () {
   getTitleInput();
   getUrlInput();
@@ -114,9 +67,25 @@ function submitBookmarkEvents () {
   getNumberOfBookmarks();
   displayNumberOfBookmarks();
   displayNumberOfUnread();
-}
-
-//add class read on button click
+};
+//function to clear read
+function clearReadBookmarks() {
+  $('.read').remove();
+};
+//enable create bookmark button when user types in title or url field
+$('#title-input, #url-input').keyup(function () {
+  $('#create-bookmark-btn').prop("disabled", false);
+});
+//button to test URL
+$('#create-bookmark-btn').on('click', function (){
+  console.log(testValidUrl())
+  if(testValidUrl()) {
+    submitBookmarkEvents()
+  } else {
+    $('.message').text('Please enter a valid URL');
+  }
+});
+//event listener to toggle class read on click
 $('.number-counter').on('click', '#markasread-btn', function (){
   $(this).parent().parent().toggleClass('read');
   if ($(this).text() === 'Mark as Read') {
@@ -128,27 +97,19 @@ $('.number-counter').on('click', '#markasread-btn', function (){
   displayNumberOfRead();
   displayNumberOfUnread();
 });
-
-//remove bookmark
+//event listener to remove bookmark on click
 $('.number-counter').on('click', '#remove-btn', function (){
   $(this).parent().parent().remove();
 displayNumberOfBookmarks();
 getNumberOfRead();
 displayNumberOfRead();
 displayNumberOfUnread();
-})
-
-
-//button to clear read bookmarks
+});
+//event listener to clear read bookmarks on click
 $('#clear-read').on('click', function () {
   clearReadBookmarks();
   getNumberOfRead();
   displayNumberOfRead();
   getNumberOfBookmarks();
   displayNumberOfBookmarks();
-})
-
-//function to clear read
-function clearReadBookmarks() {
-  $('.read').remove();
-};
+});
